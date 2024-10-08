d65fb26dd93bd90a2b1aa0648e302ac9012223db
prabashwara65 <prabashwara65@gmail.com>
2024-10-08 10:51:50 +0530

    reigister jsx is done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter , Routes , Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Register from './pages/Register'
+import Login from './pages/Login'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/Register' element={<Register />} />
+          <Route path='/Login' element={<Login />} />
         </Routes>
       </BrowserRouter>
     </>
