085bc780caccdc3e79ca12908f082b43a70954ac
Gayashaan Krishnamoorthy <gayashaan49@gmail.com>
2024-10-15 00:13:27 +0530

    making end points

--- Backend/routes/collections/wasteCollectionRoutes.js
+++ Backend/routes/collections/wasteCollectionRoutes.js
@@ -4,6 +4,7 @@ import { format } from 'mysql2';
 
 const wasteCollectionRouter = express.Router();
 
+// get all waste collection custome
 wasteCollectionRouter.get('/getAllWasteCollectionCustome', async (req, res) => {
     const db = await connectToDatabase(); // Establish the database connection
     const sql = "select w.*, c.name as collected_by, u.username as collected_from, w.date from wastecollection w, users u, collectors c where w.collected_from = u.id and w.collected_by = c.id"; // SQL query to get all bins
@@ -19,6 +20,7 @@ wasteCollectionRouter.get('/getAllWasteCollectionCustome', async (req, res) => {
     }
 });
 
+// get all waste collection
 wasteCollectionRouter.get('/getAllWasteCollection', async (req, res) => {
     const db = await connectToDatabase(); // Establish the database connection
     const sql = "select * from wastecollection"; // SQL query to get all bins
@@ -34,6 +36,7 @@ wasteCollectionRouter.get('/getAllWasteCollection', async (req, res) => {
     }
 });
 
+//count all waste
 wasteCollectionRouter.get('/countAll', async (req, res) => {
     const db = await connectToDatabase(); // Establish the database connection
     const sql = "select count(*) from wastecollection"; // SQL query to get all bins
@@ -47,7 +50,7 @@ wasteCollectionRouter.get('/countAll', async (req, res) => {
     }
 });
 
-
+//count plastic
 wasteCollectionRouter.get('/countAllPlastics', async (req, res) => {
     const db = await connectToDatabase(); // Establish the database connection
     const sql = "select count(*) from wastecollection where type='plastic'"; // SQL query to get all bins
@@ -61,6 +64,7 @@ wasteCollectionRouter.get('/countAllPlastics', async (req, res) => {
     }
 });
 
+//count food
 wasteCollectionRouter.get('/countAllFood', async (req, res) => {
     const db = await connectToDatabase(); // Establish the database connection
     const sql = "select count(*) from wastecollection where type='food'"; // SQL query to get all bins
@@ -74,6 +78,7 @@ wasteCollectionRouter.get('/countAllFood', async (req, res) => {
     }
 });
 
+//count paper
 wasteCollectionRouter.get('/countAllPaper', async (req, res) => {
     const db = await connectToDatabase(); // Establish the database connection
     const sql = "select count(*) from wastecollection where type='paper'"; // SQL query to get all bins
@@ -100,6 +105,144 @@ wasteCollectionRouter.get('/countAllEWaste', async (req, res) => {
         return res.status(500).json("Error fetching wasteData"); // Handle error
     }
 });
+
+//count plastic for one user
+wasteCollectionRouter.get('/countAllPlastics/:id', async (req, res) => {
+    const db = await connectToDatabase(); // Establish the database connection
+    const sql = "select count(*) from wastecollection w, users u where w.collected_from = u.id and w.type='plastic' and u.id = ?"; // SQL query to get all bins
+    const id = req.params.id; // Get the bin ID from request parameters
+
+    try {
+        const [rows] = await db.query(sql, [id]); // Execute the query to fetch all bins
+        return res.status(200).json({ message: "wasteData retrieved successfully", data: rows }); // Respond with the retrieved data
+    } catch (err) {
+        console.error("Error fetching bins", err);
+        return res.status(500).json("Error fetching wasteData"); // Handle error
+    }
+});
+
+//count food for one user
+wasteCollectionRouter.get('/countAllFood/:id', async (req, res) => {
+    const db = await connectToDatabase(); // Establish the database connection
+    const sql = "select count(*) from wastecollection w, users u where w.collected_from = u.id and w.type='food' and u.id = ?"; // SQL query to get all bins
+    const id = req.params.id; // Get the bin ID from request parameters
+
+    try {
+        const [rows] = await db.query(sql, [id]); // Execute the query to fetch all bins
+        return res.status(200).json({ message: "wasteData retrieved successfully", data: rows }); // Respond with the retrieved data
+    } catch (err) {
+        console.error("Error fetching bins", err);
+        return res.status(500).json("Error fetching wasteData"); // Handle error
+    }
+});
+
+//count paper for one user
+wasteCollectionRouter.get('/countAllPaper/:id', async (req, res) => {
+    const db = await connectToDatabase(); // Establish the database connection
+    const sql = "select count(*) from wastecollection w, users u where w.collected_from = u.id and w.type='paper' and u.id = ?"; // SQL query to get all bins
+    const id = req.params.id; // Get the bin ID from request parameters
+
+    try {
+        const [rows] = await db.query(sql, [id]); // Execute the query to fetch all bins
+        return res.status(200).json({ message: "wasteData retrieved successfully", data: rows }); // Respond with the retrieved data
+    } catch (err) {
+        console.error("Error fetching bins", err);
+        return res.status(500).json("Error fetching wasteData"); // Handle error
+    }
+});
+
+//count ewaste for one user
+wasteCollectionRouter.get('/countAllEWaste/:id', async (req, res) => {
+    const db = await connectToDatabase(); // Establish the database connection
+    const sql = "select count(*) from wastecollection w, users u where w.collected_from = u.id and w.type='E-Waste' and u.id = ?"; // SQL query to get all bins
+    const id = req.params.id; // Get the bin ID from request parameters
+
+    try {
+        const [rows] = await db.query(sql, [id]); // Execute the query to fetch all bins
+        return res.status(200).json({ message: "wasteData retrieved successfully", data: rows }); // Respond with the retrieved data
+    } catch (err) {
+        console.error("Error fetching bins", err);
+        return res.status(500).json("Error fetching wasteData"); // Handle error
+    }
+});
+
+
+//count all waste for one collector
+wasteCollectionRouter.get('/countAll/:id', async (req, res) => {
+    const db = await connectToDatabase(); // Establish the database connection
+    const sql = "select count(*) from wastecollection w, collectors c where w.collected_by = c.id and c.id = ?"; // SQL query to get all bins
+    const id = req.params.id; // Get the bin ID from request parameters
+
+    try {
+        const [rows] = await db.query(sql, [id]); // Execute the query to fetch all bins
+        return res.status(200).json({ message: "wasteData retrieved successfully", data: rows }); // Respond with the retrieved data
+    } catch (err) {
+        console.error("Error fetching bins", err);
+        return res.status(500).json("Error fetching wasteData"); // Handle error
+    }
+});
+
+//count plastic for one collector
+wasteCollectionRouter.get('/countAllPlastics/:id', async (req, res) => {
+    const db = await connectToDatabase(); // Establish the database connection
+    const sql = "select count(*) from wastecollection w, collectors c where w.collected_by = c.id and w.type='plastic' and c.id = ?"; // SQL query to get all bins
+    const id = req.params.id; // Get the bin ID from request parameters
+
+    try {
+        const [rows] = await db.query(sql, [id]); // Execute the query to fetch all bins
+        return res.status(200).json({ message: "wasteData retrieved successfully", data: rows }); // Respond with the retrieved data
+    } catch (err) {
+        console.error("Error fetching bins", err);
+        return res.status(500).json("Error fetching wasteData"); // Handle error
+    }
+});
+
+//count food for one collector
+wasteCollectionRouter.get('/countAllFood/:id', async (req, res) => {
+    const db = await connectToDatabase(); // Establish the database connection
+    const sql = "select count(*) from wastecollection w, collectors c where w.collected_by = c.id and w.type='food' and c.id = ?"; // SQL query to get all bins
+    const id = req.params.id; // Get the bin ID from request parameters
+
+    try {
+        const [rows] = await db.query(sql, [id]); // Execute the query to fetch all bins
+        return res.status(200).json({ message: "wasteData retrieved successfully", data: rows }); // Respond with the retrieved data
+    } catch (err) {
+        console.error("Error fetching bins", err);
+        return res.status(500).json("Error fetching wasteData"); // Handle error
+    }
+});
+
+//count paper for one collector
+wasteCollectionRouter.get('/countAllPaper/:id', async (req, res) => {
+    const db = await connectToDatabase(); // Establish the database connection
+    const sql = "select count(*) from wastecollection w, collectors c where w.collected_by = c.id and w.type='paper' and c.id = ?"; // SQL query to get all bins
+    const id = req.params.id; // Get the bin ID from request parameters
+
+    try {
+        const [rows] = await db.query(sql, [id]); // Execute the query to fetch all bins
+        return res.status(200).json({ message: "wasteData retrieved successfully", data: rows }); // Respond with the retrieved data
+    } catch (err) {
+        console.error("Error fetching bins", err);
+        return res.status(500).json("Error fetching wasteData"); // Handle error
+    }
+});
+
+//count ewaste for one collector
+wasteCollectionRouter.get('/countAllEWaste/:id', async (req, res) => {
+    const db = await connectToDatabase(); // Establish the database connection
+    const sql = "select count(*) from wastecollection w, collectors c where w.collected_by = c.id and w.type='E-Waste' and c.id = ?"; // SQL query to get all bins
+    const id = req.params.id; // Get the bin ID from request parameters
+
+    try {
+        const [rows] = await db.query(sql, [id]); // Execute the query to fetch all bins
+        return res.status(200).json({ message: "wasteData retrieved successfully", data: rows }); // Respond with the retrieved data
+    } catch (err) {
+        console.error("Error fetching bins", err);
+        return res.status(500).json("Error fetching wasteData"); // Handle error
+    }
+});
+
+
 
 
 export default wasteCollectionRouter;
